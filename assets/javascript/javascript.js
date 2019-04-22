7bce79f68ab76101360dc71524ca52d2dde8163b
mdh206 <49321381+mdharris206@users.noreply.github.com>
2019-04-22 13:56:21 -0400

    intial question list object

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,12 @@
+const questionList = [
+    {Question:"As far as has ever been reported, no-one has ever seen an ostrich bury its head in the sand?", Answer: True},
+    {Question:"Approximately one quarter of human bones are in the feet?", Answer: True},
+    {Question:"Popeye’s nephews were called Peepeye, Poopeye, Pipeye and Pupeye?",Answer: True},
+    {Question:"In ancient Rome, a special room called a vomitorium was available for diners to purge food in during meals?",Answer: False},
+    {Question:"The average person will shed 10 pounds of skin during their lifetime?",Answer: False},
+    {Question:"Sneezes regularly exceed 100 m.p.h.?",Answer: True},
+    {Question:"A slug’s blood is green?",Answer: True},
+    {Question:"The Great Wall Of China is visible from the moon?",Answer: False},
+    {Question:"Virtually all Las Vegas gambling casinos ensure that they have no clocks.",Answer: True},
+    {Question:"The total surface area of two human lungs have a surface area of approximately 70 square metres.",Answer: True},
+];
